831d66b586e3c9b15f98814a9ff9bb448e0e8be8
Charlie Vieth <charlie.vieth@gmail.com>
2017-03-16 22:31:40 -0400

    initial commit

---
+++ EventLog1/EventLogStream.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace EventLogStream
+{
+    public class EventLogStream : IDisposable
+    {
+        //
+        // Summary:
+        //     EventLog name.
+        public string LogName { get; }
+        //
+        // Summary:
+        //     Current EventLog.
+        private EventLog log;
+        //
+        // Summary:
+        //     Lock object for reading events from the EventLogEntryCollection.
+        private Object logLock = new Object();
+        //
+        // Summary:
+        //     Index of the next event in the EventLogEntryCollection.
+        private int index = 0;
+        //
+        // Summary:
+        //     Stream all events from the log, including past events.
+        private bool streamAll = true;
+        //
+        // Summary:
+        //     TextWriter for EventLogEntries.
+        private TextWriter stdout;
+        //
+        // Summary:
+        //     TextWriter for any errors encountered.
+        private TextWriter stderr;
+        //
+        // Summary:
+        //     EventLogStream run state constants.
+        private const Int32 stateStopped = 0;
+        private const Int32 stateRunning = 1;
+        private const Int32 stateDisposed = 2;
+        //
+        // Summary:
+        //     EventLogStream run state, used to prevent multiple calls to Start().
+        private Int32 state = stateStopped;
+
+        //
+        // Summary:
+        //     Marshals EventLogEntry e into UTF-8 encoded JSON.
+        //
+        // Returns:
+        //     The UTF-8 encoded JSON representaion of EventLogEntry e.
+        private string ToJson(EventLogEntry e)
+        {
+
+            using (Utf8StringWriter sw = new Utf8StringWriter())
+            {
+                using (JsonTextWriter w = new JsonTextWriter(sw))
+                {
+                    w.WriteStartObject();
+
+                    // Event log name
+                    w.WritePropertyName("LogName");
+                    w.WriteValue(LogName);
+
+                    // EventLogEntry properties
+
+                    // Looking up the category is expensive, skip it
+                    // if the CategoryNumber is 0.
+                    w.WritePropertyName("Category");
+                    if (e.CategoryNumber == 0)
+                    {
+                        w.WriteValue("(0)");
+                    } else
+                    {
+                        w.WriteValue(e.Category);
+                    }
+                    w.WritePropertyName("CategoryNumber");
+                    w.WriteValue(e.CategoryNumber);
+
+                    w.WritePropertyName("Data");
+                    w.WriteValue(e.Data);
+                    w.WritePropertyName("EntryType");
+                    w.WriteValue(e.EntryType.ToString());
+
+                    // EventID is deprecated, but include it anyway.
+#pragma warning disable CS0618 // Type or member is obsolete
+                    w.WritePropertyName("EventID");
+                    w.WriteValue(e.EventID);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+                    w.WritePropertyName("Index");
+                    w.WriteValue(e.Index);
+                    w.WritePropertyName("InstanceId");
+                    w.WriteValue(e.InstanceId);
+                    w.WritePropertyName("MachineName");
+                    w.WriteValue(e.MachineName);
+                    w.WritePropertyName("Message");
+                    w.WriteValue(e.Message);
+                    w.WritePropertyName("Source");
+                    w.WriteValue(e.Source);
+                    w.WritePropertyName("TimeGenerated");
+                    w.WriteValue(e.TimeGenerated);
+                    w.WritePropertyName("TimeWritten");
+                    w.WriteValue(e.TimeWritten);
+                    w.WritePropertyName("UserName");
+                    w.WriteValue(e.UserName ?? "");
+                    w.WriteEndObject();
+
+                    return sw.ToString();
+                }
+            }
+        }
+
+        private void StreamEvents()
+        {
+            try
+            {
+                lock (logLock)
+                {
+                    // Calling 'log.Entries.Count' is expensive, so use
+                    // two loops.  The first reduces the delta between
+                    // Count and our index, the second prints whatever
+                    // occurred during the first.
+                    int n = log.Entries.Count;
+                    while (index < n)
+                    {
+                        using (EventLogEntry entry = log.Entries[index++])
+                        {
+                            stdout.WriteLine(ToJson(entry));
+                        }
+                    }
+                    while (index < log.Entries.Count)
+                    {
+                        using (EventLogEntry entry = log.Entries[index++])
+                        {
+                            stdout.WriteLine(ToJson(entry));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                stderr.WriteLine("Error (StreamEvents): " + e.Message);
+            }
+        }
+
+        private void Callback(Object o, EntryWrittenEventArgs a)
+        {
+            // The EventLog.EntryWritten is only triggered if the last write
+            // event occured at least six seconds previously.  So we don't
+            // actually use the Entry, it only serves as a signal that writes 
+            // occured, but we should dispose of it regardless.
+            using (a.Entry)
+            {
+                try
+                {
+                    StreamEvents();
+                }
+                catch (Exception e)
+                {
+                    stderr.WriteLine("Error (Callback): " + e.Message);
+                }
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Is this necessary in C# - are reads/writes to ints <= the native word size atomic?
+            if (Interlocked.CompareExchange(ref state, stateDisposed, stateStopped) == stateStopped ||
+                Interlocked.CompareExchange(ref state, stateDisposed, stateRunning) == stateRunning)
+            {
+                log.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            // This class has no finalizer, just following the pattern...
+            GC.SuppressFinalize(this);
+        }
+
+        public void Start()
+        {
+            // Allow only one call to start.
+            if (Interlocked.CompareExchange(ref state, stateRunning, stateStopped) == stateStopped)
+            {
+                if (streamAll)
+                {
+                    StreamEvents();
+                }
+                // Register the event handler.
+                log.EntryWritten += new EntryWrittenEventHandler(Callback);
+                log.EnableRaisingEvents = true;
+            }
+            else
+            {
+                throw new Exception("EventLogStream: multiple calls to Start().");
+            }
+        }
+
+        public EventLogStream(string LogName, bool StreamAllEvents = true)
+        {
+
+            this.LogName = LogName;
+            log = new EventLog(LogName);
+
+            // This will trigger an exception if the log does not exist.
+            int x = log.Entries.Count;
+
+            streamAll = StreamAllEvents;
+            index = streamAll ? 0 : log.Entries.Count;
+
+            // TODO (CEV): Make streams configurable.
+            stdout = Console.Out;
+            stderr = Console.Error;
+        }
+    }
+}
---
+++ EventLog1/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+
+namespace EventLogStream
+{
+
+    class Program
+    {
+        //
+        // Summary:
+        //     Shutdown event used for sleeping the main thread.
+        static ManualResetEvent shutdownEvent = new ManualResetEvent(false);
+        //
+        // Summary:
+        //     Lock object for EventStreams.
+        static Object eventLock = new Object();
+        //
+        // Summary:
+        //     List of event log names to stream/read.
+        static List<string> LogNames = new List<string>();
+        //
+        // Summary:
+        //     Dictionary of EventLogStreams being streamed/read.
+        static Dictionary<string, EventLogStream> EventStreams =
+            new Dictionary<string, EventLogStream>();
+        //
+        // Summary:
+        //     Stream all events from the log, including past events.
+        static bool streamAll = false;
+        //
+        // Summary:
+        //     Exit after reading the existing logs, do not wait for 
+        //     and stream new events.
+        static bool exitAfterReading = false;
+        //
+        // Summary:
+        //     Valid command line arguments.
+        static string[] argumentNames = new string[] { "all", "exit", "help" };
+        //
+        // Summary:
+        //     Command usage message.
+        const string usageMsg = @"
+Usage EventLog [OPTIONS] LOG_NAMES
+
+  Stream event logs LOG_NAMES formatted as UTF-8 JSON to stdout.
+
+Example:
+  EventLog -all ""System"" ""Application""
+
+Options:
+  --all   Include all past events
+  --exit  Print contents of each event log then exit (implies --all)
+  --help  Display this message and exit
+";
+
+        //
+        // Summary:
+        //     Prints usage message and exits with code.
+        static void Usage(int code)
+        {
+            Console.Error.WriteLine(usageMsg);
+            System.Environment.Exit(code);
+        }
+        //
+        // Summary:
+        //     Cleans command line argument s.
+        //
+        // Returns:
+        //     Argument s converted to lowercase and stripped of 
+        //     any '--', '-' or '/' prefix.
+        static string ArgumentName(string s)
+        {
+            if (s.Length > 0)
+            {
+                if (s.StartsWith("--"))
+                {
+                    return s.Substring(2).ToLower();
+                }
+                if (s[0] == '-' || s[0] == '/')
+                {
+                    return s.Substring(1).ToLower();
+                }
+            }
+            return "";
+        }
+        //
+        // Summary:
+        //     Determines if command line argument s is a 'help' argument.
+        //
+        // Returns:
+        //     If command line argument s is a 'help' argument.
+        static bool IsHelpArgument(string s)
+        {
+            return ArgumentName(s) == "help";
+        }
+        //
+        // Summary:
+        //     Determines if command line argument s is valid.
+        //
+        // Returns:
+        //     If command line argument s is valid.
+        static bool ValidArgument(string s)
+        {
+            return argumentNames.Contains(ArgumentName(s));
+        }
+        //
+        // Summary:
+        //     Parses command line arguments args.  If an invalid or 'help' flag
+        //     is specified Usage() is be called and the program will exit.
+        static void ParseArgs(string[] args)
+        {
+            bool optionsComplete = false;
+            foreach (string arg in args.Select(s => s.Trim()))
+            {
+                if (arg == "")
+                {
+                    Console.Error.WriteLine("Ignoring empty argument");
+                    continue;
+                }
+                if (optionsComplete && arg.StartsWith("-"))
+                {
+                    // Allow "help" to be located anywhere.
+                    if (IsHelpArgument(arg))
+                    {
+                        Usage(0);
+                    }
+                    if (ValidArgument(arg))
+                    {
+                        Console.Error.WriteLine("Invalid location for argument: " + arg);
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine("Invalid argument: " + arg);
+                    }
+                    Usage(1);
+                }
+                // Like getopt, but without the terror of char arrays!!!
+                switch (arg.ToLower())
+                {
+                    case "/help":
+                    case "-help":
+                    case "--help":
+                        Usage(0);
+                        break;
+                    case "/all":
+                    case "-all":
+                    case "--all":
+                        streamAll = true;
+                        break;
+                    case "/exit":
+                    case "-exit":
+                    case "--exit":
+                        streamAll = true;
+                        exitAfterReading = true;
+                        break;
+                    default:
+                        if (arg.StartsWith("-") || arg.StartsWith("/"))
+                        {
+                            Console.Error.WriteLine("Invalid argument: " + arg);
+                            Usage(1);
+                        }
+                        if (LogNames.Exists(s => s.ToLower() == arg.ToLower()))
+                        {
+                            Console.Error.WriteLine("Ignoring duplicate Event Log: " + arg);
+                        }
+                        else
+                        {
+                            LogNames.Add(arg);
+                        }
+                        optionsComplete = true;
+                        break;
+                }
+            }
+            if (LogNames.Count == 0)
+            {
+                Console.Error.WriteLine("No Event Logs specified.");
+                Usage(1);
+            }
+        }
+
+        static void DisposeEventStreams(bool signalShutdownEvent)
+        {
+            // Lock in case this was triggered by the Exit handler
+            lock (eventLock)
+            {
+                foreach (EventLogStream log in EventStreams.Values)
+                {
+                    try
+                    {
+                        log.Dispose();
+                    }
+                    catch (Exception e)
+                    {
+                        // We are exiting anyway no need to re-throw.
+                        Console.Error.WriteLine("Error disposing of EventLogStream ({0}): {1}",
+                            log.LogName, e.Message);
+                    }
+                }
+                EventStreams.Clear();
+                if (signalShutdownEvent)
+                {
+                    shutdownEvent.Set();
+                }
+            }
+        }
+
+        protected static void ExitHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            Console.Error.WriteLine("Recieved interupt ({0}): cleaning up now", args.SpecialKey);
+            DisposeEventStreams(true);
+            Console.Error.WriteLine("Cleanup complete - exiting now");
+        }
+
+        static void Main(string[] args)
+        {
+            // Immediately set output encoding
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(ExitHandler);
+
+            // Parse command line arguments
+            ParseArgs(args);
+
+            List<Task> tasks = new List<Task>();
+            try
+            {
+                foreach (string name in LogNames)
+                {
+                    Console.WriteLine(name);
+                    EventLogStream log = new EventLogStream(name, streamAll);
+                    EventStreams.Add(name, log);
+                    tasks.Add(Task.Factory.StartNew(() => { log.Start(); }));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error initiating Event Logs: " + e.Message);
+                System.Environment.Exit(2);
+            }
+
+            try
+            {
+                Task.WaitAll(tasks.ToArray());
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error running EventLogStream tasks: " + e.Message);
+                System.Environment.Exit(2);
+            }
+
+            if (exitAfterReading)
+            {
+                DisposeEventStreams(false);
+                return;
+            }
+
+            // Wait for shutdown event, in any...
+            shutdownEvent.WaitOne();
+        }
+    }
+}
